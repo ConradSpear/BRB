5c8be5d387195d35cedc2e5ab68358d65d3784fa
Lutz <Katie.Lutz@parivedasolutions.com>
2019-01-29 22:21:19 -0600

    edits

--- appJavaScript.js
+++ appJavaScript.js
@@ -49,3 +49,8 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 }
+
+$(".dropdown-menu").on('click', 'li a', function(){
+  $(this).parent().parent().siblings(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
+  $(this).parent().parent().siblings(".btn:first-child").val($(this).text());
+});
