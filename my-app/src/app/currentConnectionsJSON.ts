dc033d182a54021e8bec61a86492d22122cc482b
Spear <Conrad.Spear@parivedasolutions.com>
2019-01-31 16:32:16 -0600

    app

--- my-app/src/app/currentConnectionsJSON.ts
+++ my-app/src/app/currentConnectionsJSON.ts
@@ -2,19 +2,19 @@ export let currentConnections = [
     {
         name: "Alex Rogers",
         email: "alex.rogers@parivedasolutions.com",
-        notes: "",
+        notes: "Speak to Alex at Cox project",
         office: "Atlanta",
         frequency: "In a week",
     },
     {
         name: "Charmaine Estacio",
         email: "charmaine.estacio@parivedasolutions.com",
-        notes: "",
-        office: "San Francisco",
+        notes: "Email Charmaine about React",
+        office: "Atlanta",
         frequency: "In 3 months",
     },
 
 
-    
+
 
 ]
