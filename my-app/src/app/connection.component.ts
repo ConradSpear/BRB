efde87acad843f841e9b825053340c9f1035ea9d
Lutz <Katie.Lutz@parivedasolutions.com>
2019-01-31 11:31:38 -0600

    added cards

--- my-app/src/app/connection.component.ts
+++ my-app/src/app/connection.component.ts
@@ -1,8 +1,10 @@
+
 import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-connection',
     templateUrl: './connection.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class ConnectionComponent {
