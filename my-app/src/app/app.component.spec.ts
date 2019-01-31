645aa10d580646f229914ca52aa409145cd86be5
Spear <Conrad.Spear@parivedasolutions.com>
2019-01-30 23:24:04 -0600

    Angular fixes

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -28,46 +28,5 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
   });
-
-  filterSelection("all");
 });
-
-
-
-
-
-function filterSelection(c) {
-debugger;
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    x[i].classList.remove("show");
-    if (x[i].className.indexOf(c) > -1) {
-      x[i].classList += " show";
-    }
-  }
-
-  active();
-}
-
-// Add active class to the current button (highlight it)
-function active() {
-
-  var btnContainer = document.getElementById("myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("btn active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-}
-/*
-$(".dropdown-menu").on('click', 'li a', function () {
-  $(this).parent().parent().siblings(".btn:first-child").html($(this).text() + ' <span class="caret"></span>');
-  $(this).parent().parent().siblings(".btn:first-child").val($(this).text());
-});*/
 
--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { City } from './city';
+import { Frequency } from './frequency';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+  modalVisible: boolean = false;
+  overLay: boolean = false;
   
+
+  ngOnInit() {
+    this.filterSelection("all")
+  }
+
+  toggleModal(){
+    this.modalVisible = !this.modalVisible;
+    var element = document.getElementById("main");
+    element.classList.toggle("overlay");
+
+    var modal = document.getElementById('myModal');
+    window.onclick = (event) => {
+      if (event.target == modal) {        
+        this.modalVisible = !this.modalVisible;
+        element.classList.toggle("overlay");
+     }
+    }
+  }
+
+  filterSelection(c) {
+
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      x[i].classList.remove("show");
+      if (x[i].className.indexOf(c) > -1) {
+        x[i].classList += " show";
+      }
+    }
+  
+    this.active();
+  }
+  
+  // Add active class to the current button (highlight it)
+  active() {
+  
+    var btnContainer = document.getElementById("myBtnContainer");
+    var btns = btnContainer.getElementsByClassName("btn");
+  
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("btn active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+  }
+
+  toggleDropDownName(){
+    var dd = document.getElementsByClassName("dropdown-menu");
+    
+    
+  
+}
+
+cities = [
+  new City(1, 'Atlanta'),
+  new City(2, 'Chicago'),
+  new City(3, 'Dallas'),
+  new City(4, 'Houston'),
+  new City(5, 'Los Angeles'),
+  new City(6, 'New York'),
+  new City(7, 'Philadelphia'),
+  new City(8, 'San Francisco'),
+  new City(9, 'Seattle'),
+  new City(10, 'Washington D.C.')
+]
+
+frequencies = [
+  new City(1, 'This Week'),
+  new City(2, 'Sometime this month'),
+  new City(3, 'Within 3 months'),
+  new City(4, 'Within 6 months'),
+  new City(5, 'Never') 
+]
+
 }
